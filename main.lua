cc046d844fa63df17d439859a0977f07c2676f8e
philbgarner <phil.b.garner@gmail.com>
2017-01-27 00:17:03 -0500

    Hooked up scenes methods to love methods.

--- main.lua
+++ main.lua
@@ -1,12 +1,25 @@
+-- Global Variables
 settings = {
     resolution = { 1024, 768 }
   }
 
+-- Modules
 anim8 = require "anim8"
-
 scenes = require "scenes"
 
+-- ******************************
+--  LOAD
+-- ******************************
 function love.load()
+  
+
+  font = love.graphics.newFont(12)
+  fontheader1 = love.graphics.newFont(24)
+  love.graphics.setFont(font)
+
+  love.physics.setMeter(1000)
+  world = love.physics.newWorld(world, 0, 0, true)
+
   
   scenes:create("overworld", {
       fnInit = function () end
@@ -16,10 +29,16 @@ function love.load()
     })
 end
 
+-- ******************************
+--  DRAW
+-- ******************************
 function love.draw()
-  
+  scenes:draw()
 end
 
+-- ******************************
+--  UPDATE
+-- ******************************
 function love.update(dt)
-  
+   scenes:update(dt)
 end
