98f2bf7a880ea1a854d63be8706364fca648246a
philbgarner <phil.b.garner@gmail.com>
2017-02-12 17:35:20 -0500

    Added a water shader (reflection + distortion) to the editor.

--- main.lua
+++ main.lua
@@ -14,6 +14,35 @@ paused = true      -- When not paused, show UI.
 -- UI methods need access to the engine object.
 ui = require "ui"
 
+
+canvas = nil
+
+time = 0
+time_dir = 1
+
+pixel = [[
+        extern float time;
+        extern float waterline;
+        extern float offsetY;
+        vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+          float factor = 0.003;
+          float water_norm = (waterline - offsetY) / 768;
+          if (texture_coords.y > water_norm)
+          {
+            vec2 tc = texture_coords;
+            
+            tc.x = tc.x + sin(tc.x * time) * factor;
+            tc.y = tc.y + cos(tc.y * time) * factor;
+              
+            return Texel(texture, vec2(tc.x, water_norm * 2 + tc.y * -1.0)) * vec4(0.5, 0.6, 1.0, 1.0);
+          }
+          else
+          {
+            return Texel(texture, texture_coords);
+          }
+        }
+      ]] 
+
 -- ******************************
 --  LOAD
 -- ******************************
@@ -25,7 +54,11 @@ function love.load()
   print("getIdentity:", love.filesystem.getIdentity( ))
   engine:create("mockup1", {0, 0, 5000, 2000})  -- mockup1 is the asset pack (folder maps to /NatureEngine/prefabs/mockup1"
                                                 -- second param is the world boundaries for the camera.
-
+  
+  shader = love.graphics.newShader(pixel)
+  shader:send("waterline", 1700)
+  
+  canvas = love.graphics.newCanvas(1024, 768)
 
 end
 
@@ -33,7 +66,18 @@ end
 --  DRAW
 -- ******************************
 function love.draw()
+  
+  love.graphics.setCanvas(canvas)
+  love.graphics.clear()
   engine:draw()
+
+  love.graphics.setCanvas()
+
+  love.graphics.setShader(shader)
+  
+    love.graphics.draw(canvas)
+  
+  love.graphics.setShader()
 
   suit:draw()
 end
@@ -42,6 +86,17 @@ end
 --  UPDATE
 -- ******************************
 function love.update(dt)
+   local cx, cy = engine.camera:getPosition()
+   cy = cy - 390
+  
+  time = time + dt * time_dir
+  if time > 10 then
+    time_dir = -1
+  elseif time < -10 then
+    time_dir = 1
+  end
+	shader:send("time", time * 9)
+	shader:send("offsetY", cy)
    
    -- UI Layout
   if paused then
